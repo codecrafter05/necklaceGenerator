8f99c9318bd2f89ba4f804d685b0a1f8f54b735c
mqarooni <qarooni97@gmail.com>
2023-09-28 23:50:03 +0300

    fixed photos path

--- Necklace/app.js
+++ Necklace/app.js
@@ -5,22 +5,19 @@ function generateImage() {
     const ctx = canvas.getContext('2d');
     
     const baseImage = new Image();
-    baseImage.src = "img/neck.png"; 
+    baseImage.src = "../img/neck.png";  // Path to the neck image
 
     baseImage.onload = function() {
-        // Draw the base image first
         ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
 
         const necklaceOverlay = new Image();
-        necklaceOverlay.src = "img/necklace.png"; 
+        necklaceOverlay.src = "../img/necklace.png";  // Path to the necklace image
 
         necklaceOverlay.onload = function() {
-            // Draw the necklace overlay
             ctx.drawImage(necklaceOverlay, 0, 0, canvas.width, canvas.height);
             
-            // Load texture for the text
             const textureImage = new Image();
-            textureImage.src = "img/gold.png"; // REPLACE with your texture path
+            textureImage.src = "../img/gold.png";  // Path to the gold texture
 
             textureImage.onload = function() {
                 const pattern = ctx.createPattern(textureImage, 'repeat');
@@ -32,5 +29,3 @@ function generateImage() {
         }
     }
 }
-
-//end ofe the code is her 
